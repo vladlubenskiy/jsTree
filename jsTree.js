8206dcc0b3e8a110505cb95e3af943c221264788
Vladislav Lubenskiy <vladislav.lubenskiy@teamdev.com>
2012-08-23 19:15:33 +0300

    Changed behavior of children processing.

--- jsTree.js
+++ jsTree.js
@@ -1,10 +1,15 @@
 var jsTreeNS = new function() {
    this.Tree = function (nodeValue, trees) {
-      if (this.validateChildren(trees)) {
-         this.children = getValue(trees);
-      // todo: It's need to reduce tree's list if it has null references.
-      } else {
-         throw "Wrong type of subtree";
+      // It's surrounded by try-catch to prevent crash of tree when any of subtree crashes.
+      try {
+         trees = getValue(trees);
+         if (this.validateChildren(trees)) {
+            this.children = trees;
+         } else {
+            throw "Wrong type of subtree";
+         }
+      } catch (error) {
+         console.log("Error while processing children of " + nodeValue);
       }
       this.nodeValue = nodeValue;
    }
@@ -12,7 +17,7 @@ var jsTreeNS = new function() {
    this.Tree.prototype.validateChildren = function(trees) {
       if (!trees) return true;
       for (var i = 0; i < trees.length; i++) {
-         if (!(trees[i] instanceof jsTreeNS.Tree) && trees[i] !== null)
+         if (!(trees[i] instanceof jsTreeNS.Tree))
             return false;
       }
       return true;
@@ -22,6 +27,8 @@ var jsTreeNS = new function() {
       return !(this.children);
    }
 
+   // I try to make it more functional.
+   // Every input data can be both value and function.
    function getValue(value) {
       return (value instanceof Function) ? value() : value;
    }
