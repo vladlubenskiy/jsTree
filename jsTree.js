937eb93fb7ad43794a54ef9c83a0e6797bc69ecd
vladlubenskiy <vlad.lubenskiy@gmail.com>
2012-08-24 02:53:17 +0300

    Changed accessibility of validateChildren()

--- jsTree.js
+++ jsTree.js
@@ -3,7 +3,7 @@ var jsTreeNS = new function() {
       // It's surrounded by try-catch to prevent crash of tree when any of subtree crashes.
       try {
          trees = getValue(trees);
-         if (this.validateChildren(trees)) {
+         if (validateChildren(trees)) {
             this.children = trees;
          } else {
             throw "Wrong type of subtree";
@@ -12,15 +12,6 @@ var jsTreeNS = new function() {
          console.log("Error while processing children of " + nodeValue);
       }
       this.nodeValue = nodeValue;
-   }
-   // Candidate to make it static
-   this.Tree.prototype.validateChildren = function(trees) {
-      if (!trees) return true;
-      for (var i = 0; i < trees.length; i++) {
-         if (!(trees[i] instanceof jsTreeNS.Tree))
-            return false;
-      }
-      return true;
    }
 
    this.Tree.prototype.isLeaf = function() {
@@ -34,6 +25,16 @@ var jsTreeNS = new function() {
          return this.nodeValue;
       }
    }
+
+   function validateChildren(trees) {
+      if (!trees) return true;
+      for (var i = 0; i < trees.length; i++) {
+         if (!(trees[i] instanceof jsTreeNS.Tree))
+            return false;
+      }
+      return true;
+   }
+   
    // I try to make it more functional.
    // Every input data can be both value and function.
    function getValue(value) {
